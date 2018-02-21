30eb12468d8cf1f8f2b92f5a55fd52be4f819245
Luz <luzma_87@yahoo.com>
2018-02-21 16:39:05 -0500

    tries to add android

--- android/app/src/main/java/com/spike/MainApplication.java
+++ android/app/src/main/java/com/spike/MainApplication.java
@@ -9,6 +9,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import org.reactnative.camera.RNCameraPackage;
+
 import java.util.Arrays;
 import java.util.List;
 
--- app/screens/VideoScreen.js
+++ app/screens/VideoScreen.js
@@ -10,6 +10,7 @@ import {
     Text, TouchableOpacity,
     View
 } from 'react-native';
+import {RNCamera} from 'react-native-camera';
 
 import styles from "../styles/main";
 
@@ -26,9 +27,13 @@ export default class MainScreen extends Component<Props> {
         this.state = {}
     }
 
-    click() {
-
-    }
+    takePicture = async function() {
+        if (this.camera) {
+            const options = { quality: 0.5, base64: true };
+            const data = await this.camera.takePictureAsync(options)
+            console.log(data.uri);
+        }
+    };
 
     render() {
         return (
@@ -36,10 +41,26 @@ export default class MainScreen extends Component<Props> {
                 <Text style={[styles.welcome, styles.redText]}>
                     !!!! {this.props.navigation.state.params.text} !!!
                 </Text>
-                <Button
-                    title="Video!"
-                    color="#aabbcc"
-                    onPress={() => this.click()}/>
+
+                <RNCamera
+                    ref={ref => {
+                        this.camera = ref;
+                    }}
+                    style={styles.preview}
+                    type={RNCamera.Constants.Type.back}
+                    flashMode={RNCamera.Constants.FlashMode.on}
+                    permissionDialogTitle={'Permission to use camera'}
+                    permissionDialogMessage={'We need your permission to use your camera phone'}
+                />
+                <View style={{flex : 0, flexDirection : 'row', justifyContent : 'center',}}>
+                    <TouchableOpacity
+                        onPress={this.takePicture.bind(this)}
+                        style={styles.capture}
+                    >
+                        <Text style={{fontSize : 14}}> SNAP </Text>
+                    </TouchableOpacity>
+                </View>
+
             </View>
         );
     }
