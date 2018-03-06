49134b9b1d328a74f2ae54479997e7798040234d
Schubert David Rodriguez <scrodrig@thoughtworks.com>
2018-03-06 12:14:00 -0500

    Add Run unit tests by GO -1 fix

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 export default class App extends React.Component {
-    render() {
-        return (
-            <View>
-                <Text>Open up App.js to start working on your app!</Text>
-                <Text>Changes you make will automatically reload. assasa</Text>
-                <Text>automatically reloads</Text>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload. assasa</Text>
+        <Text>automatically reloads</Text>
+      </View>
+    );
+  }
 }
